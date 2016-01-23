7c262399c92ba29fe7b9c6b68c8f152a1b315004
萌耳君 <199525807@qq.com>
2016-01-24 06:00:35 +0800

    ok
    图片画廊创建

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var
+gulp = require('gulp'),
+react = require('gulp-react'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+notify = require('gulp-notify'),
+browserSync = require('browser-sync'),
+reload = browserSync.reload;
+
+// sass
+gulp.task('sass', function () {
+    gulp.src('./**/css/*.scss' , { base: 'css' })
+        .pipe(sass().on('error', sass.logError))
+        //添加前缀
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('css'))
+        .pipe(reload({stream: true}))
+        .pipe(notify({ message: 'Styles task complete', sound: "Glass" }));
+});
+
+
+// react
+gulp.task('react', function () {
+    return gulp.src('./**/src/*.jsx', { base: 'js' })
+        .pipe(react())
+        .pipe(gulp.dest('js'))
+        .pipe(notify({ message: 'React task complete', sound: "Glass" }));
+});
+
+
+gulp.task('default', function() {
+
+    browserSync.init({
+        // files: "**",
+        files: ["./**/*.html","./**/*.js"],
+
+        // 动态站点
+        // proxy: "localhost:8080/xph/index.action"
+
+        // 静态站点
+        server: {
+            baseDir: "./",
+            index: "index.html"
+        }
+    })
+
+
+    gulp.watch('./**/*.scss', ['sass']);
+    gulp.watch('./**/*.jsx', ['react']);
+});
